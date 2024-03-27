c99f29eb876a14b71d9c94ad270995d664ee7458
asharaf007 <718romanempire@gmail.com>
2024-03-27 15:45:25 +0530

    first commit

---
+++ models/user.models.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type: String,
+        required:true,
+    }
+
+},{timestamps:true})
+
+export const User=mongoose.model('User',userSchema);
