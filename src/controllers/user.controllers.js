303e1b3dd64ba331feba5d87091340c11468f42d
asharaf007 <718romanempire@gmail.com>
2024-04-06 19:36:51 +0530

    Refactor user.controllers.js and user.routes.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -286,15 +286,51 @@ const getUserProfileDetails = asyncHandler(async (req, res) => {
 })
 
 const getWatchHistory = asyncHandler(async (req, res) => {
-    const username = req?.params?.username;
+    const username = req?.params;
     if (!username) {
         throw new ApiError(400, "Username is required")
     }
-    const user = await User.findOne({ username });
-    if (!user) {
-        throw new ApiError(404, "User not found")
-    }
-    return res.status(200).json(new ApiResponse(200, user.watchHistory, "Watch History found"))
+    const user = await User.aggregate([
+        {
+            $match: username
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200, user[0]?.watchHistory, "Watch history found"))
 })
 
 export { registerUser, loginUser, logoutUser, refreshAcessToken, updateAccountDetails, getCurrentUser, changeCurrentPassword, updateAvatar, updateCoverImage, getUserProfileDetails, getWatchHistory };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
+import { registerUser, loginUser, logoutUser, refreshAcessToken, updateAvatar, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateCoverImage, getUserProfileDetails, getWatchHistory } from "../controllers/user.controllers.js"
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
-import { registerUser, loginUser, logoutUser, refreshAcessToken, updateAvatar } from "../controllers/user.controllers.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
 
@@ -19,9 +19,23 @@ router.route("/register").post(
 )
 
 router.route("/login").post(loginUser);
-router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAcessToken)
 
-router.route("/update-avatar").post(verifyJWT, upload.single("avatar"), updateAvatar);
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAcessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserProfileDetails)
+
+router.route("/history/:username").get(verifyJWT, getWatchHistory)
 
 export default router;
