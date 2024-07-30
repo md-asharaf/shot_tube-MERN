cc99d01d0b310d1570b84a2d05a897ba20ced3cc
md asharaf <718romanempire@gmail.com>
2024-07-30 20:33:55 +0530

    chore: Refactor multer middleware to improve file upload handling

--- server/src/middlewares/multer.middlewares.js
+++ server/src/middlewares/multer.middlewares.js
@@ -1,24 +1,9 @@
-// import multer from "multer";
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './public/temp')
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname)
-//     }
-// })
-// export const upload = multer(
-//     {
-//         storage
-//     }
-// )
-
 
 import multer from "multer";
 
 const multipleUploadSolution = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "public/temp");
+        cb(null, "./public/temp");
     },
 
     filename: function (req, file, cb) {
