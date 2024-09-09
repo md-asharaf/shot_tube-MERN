f20258f51f5ca4512dfa3f7633d45dc347b9c248
md asharaf <718romanempire@gmail.com>
2024-09-09 19:24:32 +0530

    resolved conflicts in video.tsx page

--- client/src/services/s3.services.ts
+++ client/src/services/s3.services.ts
@@ -3,7 +3,6 @@ import {
     PutObjectCommand,
     S3Client,
 } from "@aws-sdk/client-s3";
-import AWS from "aws-sdk";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const s3Client = new S3Client({
     region: "ap-south-1",
